9eca985966ad4491b761466a43d96c0a742bcca0
Matthew Foster <matthew.foster@hakai.org>
2019-08-14 20:16:38 -0700

    Add EOV
    add eov field to schema, facets, homepage. Add code to plugin to copy key's such as display fields and categories from schema into search_facet list after search

--- ckanext/cioos_theme/helpers.py
+++ ckanext/cioos_theme/helpers.py
@@ -7,41 +7,99 @@
 '''
 
 import ckan.plugins.toolkit as toolkit
+import ckan.plugins as p
+from ckan.common import OrderedDict, _, c
+# from ckantoolkit import h
+import ckan.logic as logic
+import ckan.model as model
+from ckan.common import config
+from paste.deploy.converters import asbool
 
-def get_organization_list(data_dict):
-    '''Returns a list of organizations.
+get_action = logic.get_action
 
-    :param id: the id or name of the organization
+# def get_organization_list(data_dict):
+#     '''Returns a list of organizations.
+#
+#     :param id: the id or name of the organization
+#     '''
+#     # If a context of None is passed to the action function then the default context dict will be created
+#     # All other parameters are optional and are set to their default value
+#     # cf. http://docs.ckan.org/en/latest/extensions/plugins-toolkit.html#ckan.plugins.toolkit.ckan.plugins.toolkit.get_action
+#     return toolkit.get_action('organization_list')(None, data_dict = data_dict)
+#
+# def get_organization_dict(id):
+#     '''Returns the details of an organization.
+#
+#     :param id: the id or name of the organization
+#     '''
+#     # If a context of None is passed to the action function then the default context dict will be created
+#     # All other parameters are optional and are set to their default value
+#     # cf. http://docs.ckan.org/en/latest/api/index.html#ckan.logic.action.get.organization_show
+#     return toolkit.get_action('organization_show')(None, data_dict = {'id': id})
+#
+# def get_organization_dict_extra(organization_dict, key, default=None):
+#     '''Returns the value for the organization extra with the provided key.
+#
+#     If the key is not found, it returns a default value, which is None by
+#     default.
+#
+#     :param organization_dict: dictized organization
+#     :key: extra key to lookup
+#     :default: default value returned if not found
+#     '''
+#     extras = organization_dict['extras'] if 'extras' in organization_dict else []
+#
+#     for extra in extras:
+#         if extra['key'] == key:
+#             return extra['value']
+#
+#     return default
+
+
+def cioos_get_facets(package_type='dataset'):
+    ''' get all dataset for the given package type, including private ones.
+        This function works similarly to code found in ckan/ckan/controllers/package.py
+        in that it does a search of all datasets and populates the following
+        globals for later use:
+            c.facet_titles
+            c.search_facets
     '''
-    # If a context of None is passed to the action function then the default context dict will be created
-    # All other parameters are optional and are set to their default value
-    # cf. http://docs.ckan.org/en/latest/extensions/plugins-toolkit.html#ckan.plugins.toolkit.ckan.plugins.toolkit.get_action
-    return toolkit.get_action('organization_list')(None, data_dict = data_dict)
+    facets = OrderedDict()
 
-def get_organization_dict(id):
-    '''Returns the details of an organization.
+    default_facet_titles = {
+        'organization': _('Organizations'),
+        'groups': _('Groups'),
+        'tags': _('Tags'),
+        'res_format': _('Formats'),
+        'license_id': _('Licenses'),
+    }
 
-    :param id: the id or name of the organization
-    '''
-    # If a context of None is passed to the action function then the default context dict will be created
-    # All other parameters are optional and are set to their default value
-    # cf. http://docs.ckan.org/en/latest/api/index.html#ckan.logic.action.get.organization_show
-    return toolkit.get_action('organization_show')(None, data_dict = {'id': id})
+    for facet in toolkit.h.facets():
+        if facet in default_facet_titles:
+            facets[facet] = default_facet_titles[facet]
+        else:
+            facets[facet] = facet
 
-def get_organization_dict_extra(organization_dict, key, default=None):
-    '''Returns the value for the organization extra with the provided key.
+    # Facet titles
+    for plugin in p.PluginImplementations(p.IFacets):
+        facets = plugin.dataset_facets(facets, package_type)
 
-    If the key is not found, it returns a default value, which is None by
-    default.
+    c.facet_titles = facets
 
-    :param organization_dict: dictized organization
-    :key: extra key to lookup
-    :default: default value returned if not found
-    '''
-    extras = organization_dict['extras'] if 'extras' in organization_dict else []
+    data_dict = {
+        'facet.field': facets.keys(),
+        'rows': 0,
+        'include_private': asbool(config.get(
+            'ckan.search.default_include_private', True)),
+    }
 
-    for extra in extras:
-        if extra['key'] == key:
-            return extra['value']
+    context = {'model': model, 'session': model.Session,
+               'user': c.user, 'for_view': True,
+               'auth_user_obj': c.userobj}
 
-    return default
+    query = get_action('package_search')(context, data_dict)
+    c.search_facets = query['search_facets']
+    # return {
+    #     'search': c.search_facets,
+    #     'titles': c.facet_titles,
+    # }
--- ckanext/cioos_theme/plugin.py
+++ ckanext/cioos_theme/plugin.py
@@ -1,87 +1,146 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import ckanext.cioos_theme.helpers as OGSLhelpers #OGSL helpers
+import ckanext.cioos_theme.helpers as cioos_helpers
+from ckanext.scheming.validation import scheming_validator
 from ckan.lib.plugins import DefaultTranslation
 import json
 from shapely.geometry import shape
+import logging
+import ckan.lib.navl.dictization_functions as df
+from ckan.common import c, request
+
+
+StopOnError = df.StopOnError
+missing = df.missing
+log = logging.getLogger(__name__)
 
 
 def load_json(j):
-    return json.loads(j);
+    try:
+        new_val = json.loads(j)
+    except Exception:
+        new_val = j
+    return new_val
+
 
 def geojson_to_bbox(o):
-    return shape(o).bounds;
+    return shape(o).bounds
 
-#from the docs
-def most_popular_groups():
-    '''Return a sorted list of the groups with the most datasets.'''
 
-    # Get a list of all the site's groups from CKAN, sorted by number of
-    # datasets.
-    groups = toolkit.get_action('group_list')(
-        data_dict={'sort': 'package_count desc', 'all_fields': True})
+# from the docs
+# def most_popular_groups():
+#     '''Return a sorted list of the groups with the most datasets.'''
+#
+#     # Get a list of all the site's groups from CKAN, sorted by number of
+#     # datasets.
+#     groups = toolkit.get_action('group_list')(
+#         data_dict={'sort': 'package_count desc', 'all_fields': True})
+#
+#     # Truncate the list to the 10 most popular groups only.
+#     groups = groups[:5]
+#
+#     return groups
+#
+#
+# def groups():
+#     '''Return a sorted list of the groups'''
+#
+#     # Get a list of all the site's groups from CKAN, sorted by number of
+#     # datasets.
+#     groups = toolkit.get_action('group_list')(
+#         data_dict={'sort': 'title asc', 'all_fields': True})
+#
+#     return groups
+#
+#
+# # from the docs
+# def most_popular_datasets():
+#     '''Return a sorted list of the groups with the most datasets.'''
+#
+#     # Get a list of all the site's groups from CKAN, sorted by number of
+#     # datasets.
+#     groups = toolkit.get_action('package_search')(
+#         data_dict={'sort': 'views_recent desc', 'all_fields': True})
+#
+#     # Truncate the list to the 10 most popular groups only.
+#     groups = groups[:5]
+#
+#     return groups
+#
+#
+# def most_popular_resources():
+#     '''Return a sorted list of the groups with the most datasets.'''
+#
+#     # Get a list of all the site's groups from CKAN, sorted by number of
+#     # datasets.
+#     groups = toolkit.get_action('resource_search')(
+#         data_dict={'sort': 'views_recent desc', 'all_fields': True})
+#
+#     # Truncate the list to the 10 most popular groups only.
+#     groups = groups[:5]
+#
+#     return groups
+#
+# def recent_packages_html():
+#     '''Return a sorted list of the groups with the most datasets.'''
+#
+#     # Get a list of all the site's groups from CKAN, sorted by number of
+#     # datasets.
+#     groups = toolkit.get_action('recently_changed_packages_activity_list_html')(
+#         data_dict={'limit': '5'})
+#
+#     return groups
 
-    # Truncate the list to the 10 most popular groups only.
-    groups = groups[:5]
+# IValidators
 
-    return groups
+# this validator tries to populate eov from keywords. It looks for any english
+# keywords that match either the value or label in the choice list for the eov
+# field and add's them to the eov field.
+@scheming_validator
+def clean_and_populate_eovs(field, schema):
 
-def groups():
-    '''Return a sorted list of the groups'''
+    def validator(key, data, errors, context):
+        log.debug('eov_from_keywords_validator errors:%r', errors[key])
+        if errors[key] and (u'Select at least one' not in errors[key] or 'Select at least one' not in errors[key]):
+            return
 
-    # Get a list of all the site's groups from CKAN, sorted by number of
-    # datasets.
-    groups = toolkit.get_action('group_list')(
-        data_dict={'sort': 'title asc', 'all_fields': True})
+        keywords_main = data.get(('keywords',), {})
+        if keywords_main:
+            eov_data = keywords_main.get('en', [])
+        else:
+            extras = data.get(("__extras", ), {})
+            eov_data = extras.get('keywords-en', '').split(',')
 
-    return groups
+        eov_list = {}
+        for x in toolkit.h.scheming_field_choices(toolkit.h.scheming_field_by_name(schema['dataset_fields'], 'eov')):
+            eov_list[x['value'].lower()] = x['value']
+            eov_list[x['label'].lower()] = x['value']
 
-#from the docs
-def most_popular_datasets():
-    '''Return a sorted list of the groups with the most datasets.'''
+        d = json.loads(data.get(key, '[]'))
+        for x in eov_data:
+            val = eov_list.get(x.lower(), '')
+            if val and val not in d:
+                d.append(val)
 
-    # Get a list of all the site's groups from CKAN, sorted by number of
-    # datasets.
-    groups = toolkit.get_action('package_search')(
-        data_dict={'sort': 'views_recent desc', 'all_fields': True})
+        if len(d) and u'Select at least one' in errors[key]:
+            errors[key].remove(u'Select at least one')
+        if len(d) and 'Select at least one' in errors[key]:
+            errors[key].remove('Select at least one')
 
-    # Truncate the list to the 10 most popular groups only.
-    groups = groups[:5]
+        data[key] = json.dumps(d)
+        # log.debug('validate eov:%r', data.get(key))
+        return data
 
-    return groups
-
-#from the docs
-def most_popular_resources():
-    '''Return a sorted list of the groups with the most datasets.'''
-
-    # Get a list of all the site's groups from CKAN, sorted by number of
-    # datasets.
-    groups = toolkit.get_action('resource_search')(
-        data_dict={'sort': 'views_recent desc', 'all_fields': True})
-
-    # Truncate the list to the 10 most popular groups only.
-    groups = groups[:5]
-
-    return groups
-
-def recent_packages_html():
-    '''Return a sorted list of the groups with the most datasets.'''
-
-    # Get a list of all the site's groups from CKAN, sorted by number of
-    # datasets.
-    groups = toolkit.get_action('recently_changed_packages_activity_list_html')(
-        data_dict={'limit': '5'})
-
-    return groups
-
+    return validator
 
 
 class Cioos_ThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
-    plugins.implements(plugins.IFacets) #OSGL
-    plugins.implements(plugins.IPackageController, inherit=True) #OSGL
+    plugins.implements(plugins.IFacets)
+    plugins.implements(plugins.IPackageController, inherit=True)
+    plugins.implements(plugins.IValidators)
 
     # IConfigurer
 
@@ -91,28 +150,32 @@ class Cioos_ThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         toolkit.add_resource('fanstatic', 'cioos_theme')
 
     def get_helpers(self):
-        '''Register the most_popular_groups() function above as a template
-        helper function.
-
-        '''
+        """Register the most_popular_groups() function above as a template helper function."""
         # Template helper function names should begin with the name of the
         # extension they belong to, to avoid clashing with functions from
         # other extensions.
         return {
-	  'cioos_load_json': load_json,
-	  'cioos_geojson_to_bbox': geojson_to_bbox,
-	  'cioos_most_popular_groups': most_popular_groups,
-	  'cioos_groups': groups,
-	  'cioos_most_popular_datasets': most_popular_datasets,
-	  'cioos_most_popular_resources': most_popular_resources,
-	  'cioos_recent_packages_html': recent_packages_html,
-      'get_organization_list': OGSLhelpers.get_organization_list,
-      'get_organization_dict': OGSLhelpers.get_organization_dict,
-      'get_organization_dict_extra': OGSLhelpers.get_organization_dict_extra
-	}
+            'cioos_load_json': load_json,
+            'cioos_geojson_to_bbox': geojson_to_bbox,
+            # 'cioos_most_popular_groups': most_popular_groups,
+            # 'cioos_groups': groups,
+            # 'cioos_most_popular_datasets': most_popular_datasets,
+            # 'cioos_most_popular_resources': most_popular_resources,
+            # 'cioos_recent_packages_html': recent_packages_html,
+            'cioos_get_facets': cioos_helpers.cioos_get_facets,
+            # 'cioos_get_organization_list': cioos_helpers.get_organization_list,
+            # 'cioos_get_organization_dict': cioos_helpers.get_organization_dict,
+            # 'cioos_get_organization_dict_extra': cioos_helpers.get_organization_dict_extra
+        }
 
-    # # IFacets
-    #
+    def get_validators(self):
+        return {
+            # 'cioos_if_empty_same_as__extras': if_empty_same_as__extras,
+            'cioos_clean_and_populate_eovs': clean_and_populate_eovs,
+        }
+
+    # IFacets
+
     def dataset_facets(self, facets_dict, package_type):
         u"""Modify and return the facets_dict for the dataset search page.
 
@@ -172,37 +235,58 @@ class Cioos_ThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         :param facets_dict:
         :return:
         """
-        if 'themes' not in facets_dict:
+        if 'themes' not in facets_dict or 'eov' not in facets_dict:
             # Horrible hack
             # Insert facet themes at first position of the OrderedDict facets_dict.
             ordered_dict = facets_dict.copy()
             facets_dict.clear()
-            #facets_dict['themes'] = toolkit._('Theme')
-            #facets_dict['keywords_' + self.lang()] = toolkit._('Keywords')
+            # facets_dict['themes'] = toolkit._('Theme')
+            facets_dict['eov'] = toolkit._('Ocean Variables')
 
             for key, value in ordered_dict.items():
                 # Make translation 'on the fly' of facet tags.
                 # Should check for all translated fields.
                 # Should check translation exists.
-               if key == 'tags':
-                   facets_dict[key + '_' + self.lang()] = value
-               else:
-                   facets_dict[key] = value
-            return facets_dict
+                if key == 'tags':
+                    facets_dict[key + '_' + self.lang()] = value
+                else:
+                    facets_dict[key] = value
+        return facets_dict
 
     # IPackageController
 
+    # modfiey tags, keywords, and eov fields so that they properly index
     def before_index(self, data_dict):
         tags_dict = json.loads(data_dict.get('keywords', '{}'))
         data_dict['tags_en'] = tags_dict.get('en', [])
         data_dict['tags_fr'] = tags_dict.get('fr', [])
 
-        # if data_dict.get('extras_themes', None):
-        #     data_dict['themes'] = json.loads(data_dict.get('extras_themes', '[]'))
+        # eov is multi select so it is a json list rather then a python list
+        if(data_dict.get('eov')):
+            data_dict['eov'] = load_json(data_dict['eov'])
         return data_dict
 
-    # Custom section
+    # update eov search facets with keys from choices list in the scheming extension schema
+    def after_search(self, search_results, search_params):
+        search_facets = search_results.get('search_facets', {})
+        eov = search_facets.get('eov', {})
+        items = eov.get('items', [])
+        if items:
+            schema = toolkit.h.scheming_get_dataset_schema('dataset')
+            fields = schema['dataset_fields']
+            field = toolkit.h.scheming_field_by_name(fields, 'eov')
+            choices = toolkit.h.scheming_field_choices(field)
+            new_eovs = []
+            for item in items:
+                for c in choices:
+                    if c['value'] == item['name']:
+                        item['display_name'] = toolkit.h.scheming_language_text(c.get('label', item['name']))
+                        item['category'] = c.get('catagory', u'')
+                new_eovs.append(item)
+            search_results['search_facets']['eov']['items'] = new_eovs
+        return search_results
 
+    # Custom section
     def read_template(self):
         return 'package/read.html'
 
